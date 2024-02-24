13b021b63da715119a9e6c8796d261a4097898d5
MANOJ GORE <manojgore.mg@gmail.com>
2024-02-24 16:26:43 +0530

    ghj

--- resources/views/bill/customer_bill.blade.php
+++ resources/views/bill/customer_bill.blade.php
@@ -38,7 +38,7 @@
     <meta property="twitter:description" content="{{$metsdesc}}">
     <meta property="twitter:image" content="{{$meta_image.$meta_logo}}">
 
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script> 
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 
     <!-- Meta -->
