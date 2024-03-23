3f3a93c7be0e099dc9a48a689368b873dc3bad7e
mgore <manojgore.mg@gmail.com>
2024-03-23 15:51:19 +0530

    l

--- resources/views/customer/edit.blade.php
+++ resources/views/customer/edit.blade.php
@@ -162,7 +162,7 @@
         </div>
     @endif
 
-</div>
+</div> 
 <div class="modal-footer">
     <input type="button" value="{{__('Cancel')}}" class="btn btn-light" data-bs-dismiss="modal">
     <input type="submit" value="{{__('Update')}}" class="btn btn-primary">
