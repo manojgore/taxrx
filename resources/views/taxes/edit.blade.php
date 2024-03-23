b8f91429bf7cf6d1ec834295c9d96e752f94992c
mgore <manojgore.mg@gmail.com>
2024-03-23 15:53:43 +0530

    j

--- resources/views/taxes/edit.blade.php
+++ resources/views/taxes/edit.blade.php
@@ -2,7 +2,7 @@
 <div class="modal-body">
     <div class="row">
         <div class="form-group col-md-6">
-            {{ Form::label('name', __('Tax Rate Name'),['class'=>'form-label']) }}
+            {{ Form::label('name', __('Tax Rate Name'),['class'=>'form-label']) }} 
             {{ Form::text('name', null, array('class' => 'form-control font-style','required'=>'required')) }}
             @error('name')
             <small class="invalid-name" role="alert">
