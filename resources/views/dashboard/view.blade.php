b222a3ab74cd3bc661f6f20d4e183db73e9d979b
mgore <manojgore.mg@gmail.com>
2024-04-01 09:52:40 +0530

    nb

--- resources/views/dashboard/view.blade.php
+++ resources/views/dashboard/view.blade.php
@@ -7,7 +7,7 @@
                         <img {{ $user->img_avatar }} class="avatar rounded-circle avatar-lg"> 
                     </div>
                     <h5 class="h6 mt-4 mb-0">
-                        <p>{{ $user->name }}</p>
+                        <p>{{ $user->name }}</p> 
                     </h5>
                     <p class="d-block text-sm text-muted mb-3">{{ $user->email }}</p>
                 </div> 
