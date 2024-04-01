1cafcb863ab6c0d5ae485c9dff723790cb74eb99
mgore <manojgore.mg@gmail.com>
2024-04-01 09:54:28 +0530

    qw

--- resources/views/taxes/create.blade.php
+++ resources/views/taxes/create.blade.php
@@ -19,7 +19,7 @@
             </small> 
             @enderror
         </div>
-    </div>
+    </div> 
 </div>
 <div class="modal-footer">
     <input type="button" value="{{__('Cancel')}}" class="btn  btn-light" data-bs-dismiss="modal">
